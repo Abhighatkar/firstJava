aef2f5916c777685d6ba3282fb5148bab6787f14
Abhighatkar <2025.abhishek.ghatkar@ves.ac.in>
2026-02-11 15:01:28 +0530

    Create ReadFromFile.java

---
+++ ReadFromFile.java
@@ -0,0 +1,11 @@
+class ReadFromFile{
+  public static void main(String[] args){
+    try{
+    BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+int a = Integer.parseInt(br.readLine());
+int b = Integer.parseInt(br.readLine());
+    } catch{
+      
+    }
+  }
+}
